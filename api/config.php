cea992fde5394af439b816f2b6ea733a6e41325d
FlaimLiss <relax_men02@mail.ru>
2025-06-21 00:49:48 +0500

    Update config.php

--- api/config.php
+++ api/config.php
@@ -2,7 +2,7 @@
 $host = "sql307.infinityfree.com"; // замените на ваш хост
 $dbname = "if0_39282404_modern_home_db"; // замените на имя вашей БД
 $username = "if0_39282404"; // замените на вашего пользователя
-$password = "Wer_615243_A"; // замените на ваш пароль
+$password = "itc1wf3Vl5R"; // замените на ваш пароль
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
